8a7caf22a86d89272200254b94c00208c03d344d
jamesAbantech <james@abantech.net>
2015-01-27 22:33:30 -0500

    Testing Rotation

--- NI3DPrototype/Scripts/HandBasedActions/PinchToTranslate.js
+++ NI3DPrototype/Scripts/HandBasedActions/PinchToTranslate.js
@@ -14,17 +14,12 @@ var TranslatePinchedObject = function (hand)
 
                 action.Initialize(pinchedObject);
             }
-
-            console.log("Pinching something!")
         }
     }
     else
     {
         var indexTipPos = hand.fingers[1].tipPosition;
         var thumbTipPos = hand.fingers[0].tipPosition;
-        //var midPoint = new THREE.Vector3();
-        //midPoint.addVectors(indexTipPos, thumbTipPos);
-        //midPoint.divideScalar(2);
         pinchedObject.position.set((indexTipPos[0] + thumbTipPos[0]) / 2, (indexTipPos[1] + thumbTipPos[1]) / 2, (indexTipPos[2] + thumbTipPos[2]) / 2);
     }
 }
@@ -35,29 +30,10 @@ var EndTranslatePinchedObject = function (hand)
     {
         action.RegisterTranslation(pinchedObject)
         actionManager.ActionPerformed(action);
-
-        var rotationAction = new RotationAction();
-
-        rotationAction.Initialize(pinchedObject);
-
-        var vector = pinchedObject.position.clone().normalize();
-        var camera = window.camera.position.clone().normalize();
-
-        var axis = new THREE.Vector3(vector.x - camera.x, vector.y - camera.y, vector.z - camera.z);
-        var angle = Math.PI / 3;
-
-        var quaternion = new THREE.Quaternion();
-        quaternion.setFromAxisAngle(axis, angle);
-
-        pinchedObject.quaternion.setFromAxisAngle(axis, angle);
-
-        rotationAction.RegisterRotation(pinchedObject);
-        actionManager.ActionPerformed(rotationAction);
     }
 
     pinchedObject = null;
     action = null;
-    console.log("Gesture Ended!")
 }
 
 function getPinchedObject(hand)
