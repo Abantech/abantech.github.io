a1ef499779e4f2187e3856a68da412063bd3406f
GMelencio <greg@abantech.net>
2015-01-31 00:04:44 -0500

    Added fonts to support the text displayed and modified to make paste place object where the left index finger is at

--- NI3DPrototype/Scripts/HandBasedActions/WearableMenu.js
+++ NI3DPrototype/Scripts/HandBasedActions/WearableMenu.js
@@ -4,6 +4,14 @@
 /// <reference path="../AssetManager.js" />
 /// <reference path="MenuButtonChildOption.js" />
 
+var wearable = Wearable();
+var copiedObjects = [];
+var leftHandLastVisible = new Date();
+var leftHandRecentlyVisible = function (frame) { return (new Date() - leftHandLastVisible < 50); }
+var leftHandNotRecentlyVisible = function (frame) { return (new Date() - leftHandLastVisible > 50); }
+var lastTimeButtonPressedDictionary = {};
+var leftHandPalmPosition = new THREE.Vector3(0, 0, 0);
+
 function Wearable() {
     //Arm
     var wearable = new THREE.Object3D();
@@ -16,52 +24,10 @@ function Wearable() {
     var mesh = new THREE.Mesh(geometry, material);
     mesh.scale.set(1, 0.8, 1);
     wearable.add(mesh);
-    /*
-    //Button1 (closest to wrist)
-    geometry = new THREE.BoxGeometry(25, 25, 5);
-    material = new THREE.MeshPhongMaterial({ color: 0xff0000, ambient: 666666, side: 2, transparent: true, opacity: 0.4 });
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(20, 0, 65);
-    //mesh.rotation.z = 0.3;
-    mesh.rotation.y = 1.5;
-    mesh.name = 'button';
-    wearable.add(mesh);
-   
 
-    //Button2 (middle)
-    geometry = new THREE.BoxGeometry(25, 25, 5);
-    material = new THREE.MeshPhongMaterial({ color: 0x00ff00, ambient: 666666, side: 2, transparent: true, opacity: 0.4 });
-    material.transparent = true;
-    material.opacity = 0.3;
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(20, 0, 95);
-    mesh.rotation.y = 1.6;
-    mesh.name = 'button';
-    wearable.add(mesh);
-    
-    //Button3 (furthest from wrist)
-    geometry = new THREE.BoxGeometry(25, 25, 5);
-    material = new THREE.MeshPhongMaterial({ color: 0x000000, ambient: 666666, side: 2, transparent: true, opacity: 0.4 });
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(23, 0, 125);
-    mesh.rotation.y = 1.8;
-    mesh.name = 'button';
-    wearable.add(mesh);
-     */
     return wearable;
 }
 
-var wearable = Wearable();
-
-var leftHandLastVisible = new Date();
-
-var leftHandRecentlyVisible = function (frame) { return (new Date() - leftHandLastVisible < 50); }
-var leftHandNotRecentlyVisible = function (frame) { return (new Date() - leftHandLastVisible > 50); }
-var lastTimeButtonPressedDictionary = {};
-var leftHandPalmPosition = new THREE.Vector3(0, 0, 0);
-
-//assetManager.GetSelectedAssets();
-var copiedObjects = [];
 var pasteSelectedObjects =
     function (button) {
         console.log("Pasting " + copiedObjects.length + " copied objects");
@@ -90,13 +56,9 @@ var copyOrCutSelectedObjects =
 var createWearableButton = function (displayText, colorHex, positionX, positionY, positionZ, rotationY, onPressedCallback, isPressableCheck) {
     new menuButtonChildOption(displayText + "Button", 25, { color: colorHex, ambient: 666666, side: 2, transparent: true, opacity: 0.4 },
         function (mbco) {
-            //geometry = new THREE.BoxGeometry(25, 25, 5);
-            //material = new THREE.MeshPhongMaterial({ color: 0xff0000, ambient: 666666, side: 2, transparent: true, opacity: 0.4 });
-            //mesh = new THREE.Mesh(geometry, material);
             mbco.buttonMesh.position.set(positionX, positionY, positionZ);
-            //var text = new THREE.TextGeometry(displayText, { size: 20, height: 20, font: "optimer", weight: "bold", style: "normal" });
-            //mbco.buttonMesh.add(new THREE.Mesh(text, new THREE.MeshPhongMaterial(mbco.meshParameters)))
-            //mesh.rotation.z = 0.3;
+            var text = new THREE.TextGeometry(displayText, { size: 3, height: 1, weight: "bold" });
+            mbco.buttonMesh.add(new THREE.Mesh(text, new THREE.MeshPhongMaterial(mbco.meshParameters)))
             mbco.buttonMesh.rotation.y = rotationY;
             wearable.add(mbco.buttonMesh);
         },
@@ -115,22 +77,23 @@ var createWearableButton = function (displayText, colorHex, positionX, positionY
     );
 }
 
-createWearableButton("Paste", 0xff0000, 20, 0, 65, 1.5, pasteSelectedObjects,
+createWearableButton("Paste", 0xDBA901, 20, 0, 65, 1.5, pasteSelectedObjects,
         function (button) { return copiedObjects.length > 0; }
     );
 
-createWearableButton("Copy", 0x00ff00, 20, 0, 95, 1.6, function (button) { copyOrCutSelectedObjects(button, false); },
+createWearableButton("Copy", 0x2E2EFE, 20, 0, 95, 1.6, function (button) { copyOrCutSelectedObjects(button, false); },
         function (button) { return assetManager.GetSelectedAssets().length > 0; }
     );
 
-createWearableButton("Cut", 0x0000ff, 23, 0, 125, 1.8, function (button) { copyOrCutSelectedObjects(button, true); },
+createWearableButton("Cut", 0x7401DF, 24, 0, 125, 1.8, function (button) { copyOrCutSelectedObjects(button, true); },
         function (button) { return assetManager.GetSelectedAssets().length > 0; }
     );
 
 window.scene.add(wearable);
 
 var moveWearableWithHand = {
-    action: function (hand) {
+    action: function (hand)
+    {
         if (hand.type === 'left') {
             wearable.position.set(hand.stabilizedPalmPosition[0], hand.stabilizedPalmPosition[1], hand.stabilizedPalmPosition[2]);
             wearable.rotation.set(hand.pitch(), -hand.yaw(), hand.roll());
