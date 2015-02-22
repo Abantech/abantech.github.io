bd642432e26ecc41cd7e78657c32a42aa0dcd9c1
GMelencio <greg@abantech.net>
2015-02-21 23:56:41 -0500

    Fixed 2 issues, 1- preventing two hands from showing fingers. 2 - clearing the phalanges after hand is no longer visible

--- NI3DPrototype/Scripts/FrameActions/CustomHand.js
+++ NI3DPrototype/Scripts/FrameActions/CustomHand.js
@@ -8,13 +8,10 @@ var handMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff, transparent:
 
 var Handy = function () {
     var handy = this;
-    //var msg = handData.appendChild(document.createElement('div'));
-    var geometry = new THREE.BoxGeometry(35, 10, 40);
+    var geometry = new THREE.BoxGeometry(35, 8, 40);
     var box = new THREE.Mesh(geometry, handMaterial);
     scene.add(box);
     handy.outputData = function (id, hand) {
-        //msg.innerHTML = 'Hand id:' + id + ' x:' + hand.stabilizedPalmPosition[0].toFixed(0) +
-        //    ' y:' + hand.stabilizedPalmPosition[1].toFixed(0) + ' z:' + hand.stabilizedPalmPosition[2].toFixed(0);
         box.position.set(hand.palmPosition[0], hand.palmPosition[1], hand.palmPosition[2]);
         box.rotation.set(hand.pitch(), -hand.yaw(), hand.roll());
         handy.hand = hand;
@@ -26,20 +23,18 @@ var Handy = function () {
 var Fingerling = function () {
     var fingerling = this;
     var phalanges = [], phalange;
-    for (var i = 0; i < 4; i++) {
+
+    for (var i = 0; i < 4; i++)
+    {
         phalange = addPhalange();
         phalanges.push(phalange)
     }
 
     fingerling.phalanges = phalanges;
-    //var msg = fingerData.appendChild(document.createElement('div'));
+
+    fingerling.lastSeen = null;;
 
     fingerling.outputData = function (id, finger) {
-        //msg.innerHTML = 'Bone Method ~ ' +
-        //    'finger tip: ' + id + ' x:' + finger.tipPosition[0].toFixed(0) +
-        //    ' y:' + finger.tipPosition[1].toFixed(0) + ' z:' + finger.tipPosition[2].toFixed(0);
-
-        //console.log( finger );
         var bone, cen, len;
         for (var i = 0; i < 4; i++) {
             bone = finger.bones[i];
@@ -48,46 +43,61 @@ var Fingerling = function () {
             phalange = phalanges[i];
             phalange.position.set(cen[0], cen[1], cen[2]);
             if (id > 0 || i > 0) {
-                phalange.scale.z = len;
+                phalange.scale.z = len/6;
             }
 
-            fingerling.finger = finger;
+            phalange.visible = true;
+            //fingerling.finger = finger;
         }
 
         // Eventually will look at using bone.basis XYZ-axis data; Will it produce more concise code?
+        phalanges[3].lookAt(new THREE.Vector3().fromArray(finger.tipPosition));
+        phalanges[2].lookAt(new THREE.Vector3().fromArray(finger.dipPosition));
+        phalanges[1].lookAt(new THREE.Vector3().fromArray(finger.pipPosition));
 
-        phalanges[3].lookAt(v(finger.tipPosition[0], finger.tipPosition[1], finger.tipPosition[2]));
-        phalanges[2].lookAt(v(finger.dipPosition[0], finger.dipPosition[1], finger.dipPosition[2]));
-        phalanges[1].lookAt(v(finger.pipPosition[0], finger.pipPosition[1], finger.pipPosition[2]));
-        if (id > 0) {
-            phalanges[0].lookAt(v(finger.mcpPosition[0], finger.mcpPosition[1], finger.mcpPosition[2]));
-        }
+        //if (id > 0) {
+        //    phalanges[0].lookAt(new THREE.Vector3().fromArray(finger.mcpPosition));
+        //}
         fingerling.finger = finger;
+        fingerling.lastSeen = new Date();
     };
+
+    
 };
 
 function addPhalange() {
-    geometry = new THREE.BoxGeometry(8, 8, 1);
-    //geometry = new THREE.CylinderGeometry(4, 4, 8);
+    geometry = new THREE.SphereGeometry(4, 16, 16);
     phalange = new THREE.Mesh(geometry, handMaterial);
     scene.add(phalange);
     return phalange;
 }
 
-function v(x, y, z) { return new THREE.Vector3(x, y, z); }
 
-
-var DisplayHand = function (frame) {
-    frame.hands.forEach(function (hand, id) {
-        var handy = (handies[id] || (handies[id] = new Handy()));
-        handy.outputData(id, hand);
-        hand.fingers.forEach(function (finger, id) {
-            var fingerling = (fingerlings[id] || (fingerlings[id] = new Fingerling()));
-            fingerling.outputData(id, finger);
+frameActions.RegisterAction("DisplayHands",
+    function (frame) {
+        frame.hands.forEach(function (hand, id) {
+            //var handy = (handies[id] || (handies[id] = new Handy()));
+            //handy.outputData(id, hand);
+            hand.fingers.forEach(
+                function (finger, id)
+                {
+                    var fingerlingID = hand.type + '-' + id;
+                    var fingerling = (fingerlings[fingerlingID] || (fingerlings[fingerlingID] = new Fingerling()));
+                    fingerling.outputData(fingerlingID, finger);
+                }
+            );
         });
+
+        var currentTime = new Date();
+
+        for (var obj in fingerlings)
+        {
+            if ((currentTime - fingerlings[obj].lastSeen) > 10)
+            {
+                fingerlings[obj].phalanges.forEach(
+                    function (phalange) {
+                        phalange.visible = false;
+                    })
+            }
+        }
     });
-}
-
-
-//inject into frameActions
-frameActions.RegisterAction("DisplayHands", DisplayHand);
