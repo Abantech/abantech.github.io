50d2fa5d386b76167514abe2957ed17993a8bed2
GMelencio <greg@abantech.net>
2015-02-27 23:21:52 -0500

    updated default position and scale in transformplugin so that use of handsPositionAdjustment is not necessary

--- NI3DPrototype/Scripts/LeapLoopSetup-NoHandPlugins.js
+++ NI3DPrototype/Scripts/LeapLoopSetup-NoHandPlugins.js
@@ -2,31 +2,21 @@
         function (frame) {
             frame.hands.forEach(
               function (hand) {
-                  if (handController)
+                  if (typeof (handcontroller) != 'undefined' && handController)
                       handController.RunAllActions(hand);
               }
             )
 
-            if (frameActions)
+            if (typeof (frameActions) != 'undefined' && frameActions)
                 frameActions.RunAllActions(frame);
         }
     )
 // note that transform must be _before_ rigged hand
 .use('transform', {
     quaternion: new THREE.Quaternion,
-    position: new THREE.Vector3,
-    scale: .55
+    position: new THREE.Vector3(0,-50,0),
+    scale: .5
 })
-/*
-.use('riggedHand', {
-    dotsMode: false,
-    parent: scene,
-    renderFn: function () {
-        renderer.render(scene, camera);
-    }
-
-})
-*/
 .connect();
 
 window.transformPlugin = Leap.loopController.plugins.transform;
