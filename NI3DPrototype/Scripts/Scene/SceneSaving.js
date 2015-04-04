a54f951a26d063f232d2c5922df549baacc58363
GMelencio <greg@abantech.net>
2015-04-04 11:50:40 -0400

    Scene saving of only assets

--- NI3DPrototype/Scripts/Scene/SceneSaving.js
+++ NI3DPrototype/Scripts/Scene/SceneSaving.js
@@ -9,3 +9,18 @@ function importScene(json)
 {
     scene = new THREE.ObjectLoader().parse(JSON.parse(json));
 }
+
+function exportSceneAssets()
+{
+    var exportedScene = new THREE.Scene();
+    for (var i = 0; i < scene.children.length; i++) 
+    {
+        var sceneObject = scene.children[i];
+        if (sceneObject.userData.isAsset)
+        {
+            exportedScene.add(sceneObject);
+        }
+    }
+
+    return JSON.stringify(exportedScene.toJSON(), null, '\t');
+}
