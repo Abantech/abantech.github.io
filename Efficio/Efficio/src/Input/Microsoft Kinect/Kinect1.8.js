8af5243e99984c5f9189857addb2dc2118ac0ad7
IsmaelV1 <ismael@abantech.com>
2015-11-26 10:52:48 -0500

    Added the Kinect Reader

---
+++ Efficio/Efficio/src/Input/Microsoft Kinect/Kinect1.8.js
@@ -0,0 +1,54 @@
+﻿define(['postal'], function (bus) {
+    var source = 'Kinect';
+    var trackingType = 'Seated';
+    var controller;
+    var device = "Kinect1.8";
+
+
+    return {
+        Initialize: function () {
+
+            // Create Controller
+            controller = new WebSocket("ws://localhost:8181");
+
+            // Sends message when controller is connected
+            controller.onopen = function () {
+                bus.publish({
+                    channel: 'Devices',
+                    topic: 'Connected',
+                    source: source,
+                    data: {
+                        name: source,
+                        device: "Kinect",
+                        controller: controller,
+                        test: 'test'
+                    }
+                });
+            };
+
+            // Connection closed.
+            controller.onclose = function () {
+                console.log("Connection closed.");
+            }
+        },
+
+        Start: function () {
+
+            // Listens for input from device
+            controller.onmessage = function (frame) {
+                // SKELETON DATA
+                var jsonObject = JSON.parse(frame.data);
+
+                bus.publish({
+                    channel: 'Input.Raw',
+                    topic: device,
+                    source: source,
+                    data: {
+                        trackingType: trackingType,
+                        input: frame
+                    }
+                });
+            };
+        }
+    }
+});
