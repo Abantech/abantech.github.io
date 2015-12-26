db15b14229bf9cc91ab66793f181a6a4a51b45ad
James Stephens <james.abantech@outlook.com>
2015-12-26 17:15:42 -0500

    Continued pushes

--- AbantechSite/js/RequireConfiguration.js
+++ AbantechSite/js/RequireConfiguration.js
@@ -1,6 +1,6 @@
 ﻿var require = {
     //By default load any module IDs from js/lib
-    baseUrl: '../',
+    baseUrl: '.',
     //except, if the module ID starts with "app",
     //load it from the js/app directory. paths
     //config is relative to the baseUrl, and
@@ -12,11 +12,16 @@
         THREE: 'http://threejs.org/build/three.min',
         leapjs: 'https://cdn.rawgit.com/leapmotion/leapjs/master/leap-0.6.4.min',
         annyang: 'https://cdn.rawgit.com/TalAter/annyang/master/annyang',
-        gyronorm: 'https://cdn.rawgit.com/dorukeker/gyronorm.js/master/dist/gyronorm.complete.min'
+        gyronorm: 'https://cdn.rawgit.com/dorukeker/gyronorm.js/master/dist/gyronorm.complete.min',
+        promise: 'https://www.promisejs.org/polyfills/promise-6.1.0',
+        autobahn: 'https://autobahn.s3.amazonaws.com/autobahnjs/latest/autobahn.min',
+        realsense: 'js/lib/realsense'
     },
     shim: {
         'leapjs': { exports: 'Leap' },
         'annyang': { exports: 'annyang' },
-        'gyronorm': { exports: 'GyroNorm' }
+        'gyronorm': { exports: 'GyroNorm' },
+        'realsense': { exports: 'intel.realsense' },
+        'autobahn': { exports: 'autobahn' }
     }
 };
