8d341cee9e414f519be82d1f90005cd47c87f240
James Stephens <james.abantech@outlook.com>
2015-12-30 15:28:09 -0500

    Shell for KinectBroadcaster

---
+++ Efficio/Server Side/DeviceBroadcaster/Devices/Microsoft/KinectBroadcaster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceBroadcaster.Devices.Microsoft
+{
+    class KinectBroadcaster : IBroadcaster
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartBroadcast()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
