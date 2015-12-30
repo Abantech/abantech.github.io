b591e0690cd8efa5d7289c1a6bce54d72520bff9
jamesAbantech <james@abantech.net>
2015-12-30 16:48:17 -0500

    Forgot some files

---
+++ Efficio/Client Side/Efficio/src/Input Recognition and Processing/Device Input Recognition and Processing/DeviceInputRecognitionAndProcessing.js
@@ -0,0 +1,47 @@
+﻿define(['postal'], function (bus) {
+
+    var dipr = { PrototypeExtensions: {}, };
+
+    function Initialize(EfficioConfiguration) {
+        // Extend input models for easier processing and to make them more informative
+
+        require(['Input Recognition and Processing/Device Input Recognition and Processing/CustomDeviceLibrary'], function (customGestureLibraries) {
+            dipr.CustomGestureLibrary = customGestureLibraries.Initialize();
+
+            bus.subscribe({
+                channel: "Input.Raw.Device",
+                topic: "*",
+                callback: function (data, envelope) {
+                    customGestureLibraries.ProcessInput(data, envelope);
+                }
+            });
+        }), function (err) {
+            bus.publish({
+                channel: 'UserNotification',
+                topic: 'Warn',
+                source: source,
+                data: {
+                    message: 'No custom gesture library configured'
+                }
+            });
+        };
+
+        require(['Input Recognition and Processing/Device Input Recognition and Processing/EfficioDeviceGrimoire'], function (efficioGestureLibrary) {
+            dipr.EfficioGestureLibrary = efficioGestureLibrary.Initialize();
+
+            bus.subscribe({
+                channel: "Input.Raw.Device",
+                topic: "*",
+                callback: function (data, envelope) {
+                    efficioGestureLibrary.ProcessInput(data, envelope);
+                }
+            });
+        });
+
+        return dipr;
+    }
+
+    return {
+        Initialize: Initialize
+    }
+});
---
+++ Efficio/Client Side/Efficio/src/Input Recognition and Processing/Device Input Recognition and Processing/Orientation/Orientation.js
@@ -0,0 +1,49 @@
+﻿define(['postal'], function (bus) {
+
+    var source = 'Efficio Device Grimoire';
+    var channel = 'Input.Processed.Efficio.Device';
+    var dictionary = 'Device';
+    var trackingType = 'Orientation';
+    var orientationProcessor;
+
+    function RawOrientationData(data) {
+        var gestureName = "RawOrientationData";
+        var gestureInformation = Efficio.InputAndGestureRecognition.ActiveGesturesDictionary.CreateOrUpdateEntry(trackingType, gestureName, dictionary)
+
+        bus.publish({
+            channel: channel,
+            topic: gestureName,
+            source: source,
+            data: {
+                Input: data,
+                GestureInformation: gestureInformation
+            }
+        });
+    };
+
+        function ProcessInput(data) {
+
+        if (!orientationProcessor) {
+            orientationProcessor = {
+                Name: name,
+                TrackingType: trackingType,
+                Dictionary: dictionary,
+                Positions: {
+                    RawOrientationData: RawOrientationData,
+                }
+            }
+        }
+
+        for (var position in orientationProcessor.Positions) {
+            orientationProcessor.Positions[position](data);
+        }
+
+        return orientationProcessor;
+
+    }
+
+
+    return {
+        ProcessInput: ProcessInput
+    }
+})
