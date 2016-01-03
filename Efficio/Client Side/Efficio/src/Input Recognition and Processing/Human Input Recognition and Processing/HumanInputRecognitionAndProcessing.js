135f19a77bc473042dbc1ea2a692c62b94dc69ac
James Stephens <james.abantech@outlook.com>
2016-01-03 17:20:24 -0500

    Adding Missing files

---
+++ Efficio/Client Side/Efficio/src/Input Recognition and Processing/Human Input Recognition and Processing/HumanInputRecognitionAndProcessing.js
@@ -0,0 +1,71 @@
+﻿define(['postal'], function (bus) {
+
+    var hipr = { PrototypeExtensions: {}, };
+
+    function Initialize(EfficioConfiguration) {
+        // Extend input models for easier processing and to make them more informative
+
+        if (Efficio.Configuration.Devices.LeapMotion) {
+            (function LeapMotionPrototypeExtensions() {
+                hipr.PrototypeExtensions.LeapMotion = {};
+
+                require(['Input Recognition and Processing/Human Input Recognition and Processing/Input Extensions/LeapMotion/HandExtensions'], function (handExtensions) {
+                    hipr.PrototypeExtensions.LeapMotion.Hand = handExtensions.ExtendClasses();
+                });
+
+                require(['Input Recognition and Processing/Human Input Recognition and Processing/Input Extensions/LeapMotion/FingerExtensions'], function (fingerExtensions) {
+                    hipr.PrototypeExtensions.LeapMotion.Finger = fingerExtensions.ExtendClasses();
+                });
+            })();
+        }
+
+        if (Efficio.Configuration.Devices.RealSense) {
+            (function RealSensePrototypeExtensions() {
+                hipr.PrototypeExtensions.RealSense = {};
+
+                require(['Input Recognition and Processing/Human Input Recognition and Processing/Input Extensions/Intel/RealSense/HandExtensions'], function (handExtensions) {
+                    hipr.PrototypeExtensions.RealSense.Hand = handExtensions.ExtendClasses();
+                });
+            })();
+        }
+
+        require(['Input Recognition and Processing/Human Input Recognition and Processing/CustomGestureLibrariesAccess'], function (customGestureLibraries) {
+            hipr.CustomGestureLibrary = customGestureLibraries.Initialize();
+
+            bus.subscribe({
+                channel: "Input.Raw.Human",
+                topic: "*",
+                callback: function (data, envelope) {
+                    customGestureLibraries.ProcessInput(data, envelope);
+                }
+            });
+        }), function (err) {
+            bus.publish({
+                channel: 'UserNotification',
+                topic: 'Warn',
+                source: source,
+                data: {
+                    message: 'No custom gesture library configured'
+                }
+            });
+        };
+
+        require(['Input Recognition and Processing/Human Input Recognition and Processing/EfficioGestureGrimoire'], function (efficioGestureGrimoire) {
+            hipr.EfficioGestureGrimoire = efficioGestureGrimoire.Initialize();
+
+            bus.subscribe({
+                channel: "Input.Raw.Human",
+                topic: "*",
+                callback: function (data, envelope) {
+                    efficioGestureGrimoire.ProcessInput(data, envelope);
+                }
+            });
+        });
+
+        return hipr;
+    }
+
+    return {
+        Initialize: Initialize
+    }
+});
---
+++ Efficio/Client Side/Efficio/src/Input Recognition and Processing/Human Input Recognition and Processing/Input Extensions/Intel/RealSense/HandExtensions.js
@@ -0,0 +1,141 @@
+﻿define(['realsense', 'Helpers/Math'], function (realsense, math) {
+
+    function GetSide(hand) {
+        var hand = hand || this;
+
+        hand.bodySide === 2 ? 'Right' : 'Left';
+    }
+
+    function IsFlexed(hand) {
+        var hand = hand || this;
+        return FlexionAngle(hand) > hand.FlexedAngle;
+    }
+
+    function FlexionAngle(hand) {
+        var hand = hand || this;
+
+        //TODO Find flexion angle
+    }
+
+    function IsExtended(hand) {
+        var hand = hand || this;
+        return ExtensionAngle(hand) > hand.ExtendedAngle;
+    }
+
+    function ExtensionAngle(hand) {
+        var hand = hand || this;
+
+        // TODO find extensions angle
+    }
+
+    function IsSupine(hand) {
+        var hand = hand || this;
+
+        return SupinationAngle(hand) > hand.SupineAngle;
+    }
+
+    function SupinationAngle(hand) {
+        var hand = hand || this;
+
+        // TODO find supination angle
+    }
+
+    function IsNeutral(hand) {
+        var hand = hand || this;
+
+        return !IsSupine(hand) && !IsProne(hand) && !IsHyperRotated(hand);
+    }
+
+    function IsProne(hand) {
+        var hand = hand || this;
+
+        return PronationAngle(hand) > -hand.ProneAngleTolerance && PronationAngle(hand) < hand.ProneAngleTolerance;
+    }
+
+    function PronationAngle(hand) {
+        var hand = hand || this;
+
+        // TODO find pronation angle
+    }
+
+    function IsHyperRotated(hand) {
+        var hand = hand || this;
+        var measure = HyperRotationAngle(hand);
+
+        return measure < -hand.ProneAngleTolerance;
+    }
+
+    function HyperRotationAngle(hand) {
+        var hand = hand || this;
+
+        //TODO find hyper rotation angle
+    }
+
+    function IsUlnarDeviated(hand) {
+        var hand = hand || this;
+        return DeviatedMeasurment(hand) > hand.UlnarDeviatedMeasure;
+    }
+
+    function IsRadialDeviated(hand) {
+        var hand = hand || this;
+        return DeviatedMeasurment(hand) < hand.RadialDeviatedMeasure;
+    }
+
+    function DeviatedMeasurment(hand) {
+        var hand = hand || this;
+
+        // TODO find deviation measure
+    }
+
+    function ExtendClasses() {
+        var extensions = {
+            GetSide: GetSide,
+
+            // Flex Helpers
+            IsFlexed: IsFlexed,
+            FlexedAngle: 30,
+            FlexionAngle: FlexionAngle,
+
+            // Extension Helpers
+            IsExtended: IsExtended,
+            ExtendedAngle: 30,
+            ExtensionAngle: ExtensionAngle,
+
+            // Supination
+            IsSupine: IsSupine,
+            SupineAngle: 145,
+            SupinationAngle: SupinationAngle,
+
+            // Neutral
+            IsNeutral: IsNeutral,
+
+            // Pronation
+            IsProne: IsProne,
+            ProneAngleTolerance: 15,
+            PronationAngle: PronationAngle,
+
+            // Hyper Rotation
+            IsHyperRotated: IsHyperRotated,
+            HyperRotationAngle: HyperRotationAngle,
+
+            // Ulnar Deviation
+            IsUlnarDeviated: IsUlnarDeviated,
+            UlnarDeviatedMeasure: .5,
+            DeviatedMeasurment: DeviatedMeasurment,
+
+            // Radial Deviation
+            IsRadialDeviated: IsRadialDeviated,
+            RadialDeviatedMeasure: -.2,
+        }
+
+        for (var property in extensions) {
+            intel.realsense.hand.HandData.IHand.prototype[property] = extensions[property];
+        }
+
+        return extensions;
+    }
+
+    return {
+        ExtendClasses: ExtendClasses
+    }
+})
