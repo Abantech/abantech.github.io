c839f79a1eb4dd46a610e0588abc848896795b5e
GMelencio <greg@abantech.net>
2016-03-14 14:40:45 -0400

    Added sample code for using the DeviceBroadcaster

--- Farside Integration/AutoDesk/AutoCADEndUser/scripts/EfficioIntegration/Configuration/EfficioConfiguration.js
+++ Farside Integration/AutoDesk/AutoCADEndUser/scripts/EfficioIntegration/Configuration/EfficioConfiguration.js
@@ -11,3 +11,13 @@
 
     ActionToFunctionMapping: ActionToFunctionMapping
 }
+
+/*
+To use broadcaster use:
+
+        LeapMotion: {
+            Host: "127.0.0.1", // replace with external IP address
+            port: 3003, // make sure this port is open on firewall/router
+            EnableGestures: true
+        }
+*/
