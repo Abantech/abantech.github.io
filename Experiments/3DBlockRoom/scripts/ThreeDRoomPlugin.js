536677824684113fbfd30f4d318ede726fe6f3b2
GMelencio <greg@abantech.net>
2016-03-27 11:16:31 -0400

    Targetted cube follows position of invisible target ball

--- Experiments/3DBlockRoom/scripts/ThreeDRoomPlugin.js
+++ Experiments/3DBlockRoom/scripts/ThreeDRoomPlugin.js
@@ -5,6 +5,7 @@ var raycaster = new THREE.Raycaster();
 var counter = 0;
 var targetBall;
 var targetBallDistance = 10;
+var selectedCube;
 
 ThreeDRoomPlugin = {
     Ready: function (data) {
@@ -32,6 +33,29 @@ ThreeDRoomPlugin = {
                 targetBall.position = new THREE.Vector3( 0, 0, targetBallDistance );
             }
 
+            if ( typeof ( selectedCube ) === "undefined" )
+            {
+                rayCenter.x = 0;
+                rayCenter.y = 0;
+                raycaster.setFromCamera( rayCenter, camera );
+                raycaster.far = 5000;
+                var intersects = raycaster.intersectObjects( scene.children );
+                var intersectedObjectsCount = intersects.length;
+                for ( var i = 0; i < intersectedObjectsCount; i++ )
+                {
+                    document.getElementById( "info" ).innerHTML = "Intersected object with ID " + intersects[i].object.id;
+                    if ( intersects[i].object.isAsset )
+                    {
+                        intersects[i].object.material.color.set( 0xff0000 );
+                        selectedCube = intersects[i].object;
+                    }
+                    //document.getElementById( "info" ).innerHTML = intersectedObjectsCount > 0 ? "Nothing intersected" : "INTERSECTED " + intersectedObjectsCount + " OBJECTS"
+                }
+            }
+            else
+            {
+                selectedCube.position.copy( targetBall.position );
+            }
             //var message = typeof(camera) === "undefined" ? "NOT defined" : "DEFINED";
             document.getElementById( "info" ).innerHTML = "You have VALIDATED camera " + message + " " + counter++ + " times"
         }
